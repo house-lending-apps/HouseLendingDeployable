82fcd3aa16004eadf030564329ecd897b44325fd
Mahesh Kulkarni <kulkarni.mak172@gmail.com>
2016-06-22 22:06:22 +0200

    updated script

--- install-application.sh
+++ install-application.sh
@@ -8,12 +8,12 @@ export mongodbURL="mongodb://localhost:27017/house-lending-db"
 
 echo "Home directory is: $HOME"
 echo "install-application.sh: directory = $PWD"
-echo "REPOSITORY_NAME = https://b18290981a67975fe2378f54cdcce06f95aff077@github.com/house-lending-apps/HouseLendingMain.git"
+echo "REPOSITORY_NAME = https://github.com/house-lending-apps/HouseLendingMain.git"
 echo "targetBranch = master"
 
-echo "Starting to pull latest ... "
+echo "Starting to pull latest ... from $REPOSITORY_NAME "
 
-git clone $REPOSITORY_NAME $targetBranch
+git pull
 
 npm install
 
